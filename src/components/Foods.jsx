b1fdb7f37397d6d75c89090f451f0ae223eedec3
Ahmed Shakir <ahmed.shakir@supernovait.se>
2021-10-21 15:15:38 +0200

    feat: add pagination component

--- src/components/Foods.jsx
+++ src/components/Foods.jsx
@@ -3,6 +3,7 @@ import { getFoods } from "../services/fakeFoodService";
 import { deleteData, getData, storeData } from '../services/localStorageService';
 import AddFood from './AddFood';
 import Food from './Food';
+import Pagination from './Pagination';
 
 class Foods extends Component {
   static LOCAL_STORAGE_KEY = "foods";
@@ -11,13 +12,15 @@ class Foods extends Component {
     super();
     this.state = {
       foods: [],
+      pagination: this.setPagination(0, 5, 1),
       isAddingNew: false
     };
   }
 
   componentDidMount() {
     const foods = this.loadData();
-    this.setState({foods});
+    const pagination = this.setPagination(foods.length, 5, 1);
+    this.setState({ foods, pagination });
   }
 
   handleNewFood = () => {
@@ -26,6 +29,7 @@ class Foods extends Component {
 
   handleSave = (food) => {
     const foods = [...this.state.foods];
+    const pagination = this.setPagination(foods.length, 5, 1);
 
     if (foods.find((f) => f._id === food._id) === undefined) {
       foods.push(food);
@@ -35,7 +39,7 @@ class Foods extends Component {
       foods[index].isEditing = false;
     }
     this.saveData(foods);
-    this.setState({ foods: foods, isAddingNew: false });
+    this.setState({ foods: foods, pagination: pagination, isAddingNew: false });
   };
 
   handleEdit = (food) => {
@@ -56,8 +60,9 @@ class Foods extends Component {
 
   handleDelete = (food) => {
     const foods = this.state.foods.filter((f) => f._id !== food._id);
+    const pagination = this.setPagination(foods.length, 5, 1);
     this.saveData(foods);
-    this.setState({ foods });
+    this.setState({ foods, pagination });
   };
 
   handleLike = (food) => {
@@ -67,6 +72,11 @@ class Foods extends Component {
     foods[index].isLiked = !food.isLiked;
     this.saveData(foods);
     this.setState({ foods });
+  };
+
+  handlePageChange = (currentPage) => {
+    const pagination = this.setPagination(this.state.foods.length, 5, currentPage);
+    this.setState({ pagination });
   };
 
   handleDataReset = () => {
@@ -83,6 +93,18 @@ class Foods extends Component {
 
   saveData(data) {
     storeData(Foods.LOCAL_STORAGE_KEY, data);
+  }
+
+  getPaginatedFoods() {
+    const {currentPage, itemsPerPage} = this.state.pagination;
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return this.state.foods.filter((food, i) => i >= startIndex && i < endIndex);
+  }
+
+  setPagination(numOfItems, itemsPerPage, currentPage) {
+    const numOfPages = Math.ceil(numOfItems /itemsPerPage);
+    return {numOfItems, numOfPages, itemsPerPage, currentPage};
   }
 
   getNoDataView() {
@@ -108,31 +130,33 @@ class Foods extends Component {
           </nav>
 
           {this.state.foods.length === 0 ? this.getNoDataView() : (
-
-            <table className="table table-hover">
-              <thead>
-                <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">Category</th>
-                  <th scope="col">Stock</th>
-                  <th scope="col">Price</th>
-                  <th scope="col" />
-                  <th scope="col">
-                    <button onClick={this.handleNewFood} className="btn btn-success btn-sm m-1" title="add new entry"><i className="fas fa-plus" aria-hidden="true" /></button>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.foods.map((food) => (
-                  <Food key={food._id}
-                    data={food}
-                    onSave={this.handleSave}
-                    onEdit={this.handleEdit}
-                    onDelete={this.handleDelete}
-                    onCancel={this.handleCancel}
-                    onLike={this.handleLike} />))}
-              </tbody>
-            </table>
+            <>
+              <table className="table table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Category</th>
+                    <th scope="col">Stock</th>
+                    <th scope="col">Price</th>
+                    <th scope="col" />
+                    <th scope="col">
+                      <button onClick={this.handleNewFood} className="btn btn-success btn-sm m-1" title="add new entry"><i className="fas fa-plus" aria-hidden="true" /></button>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.getPaginatedFoods().map((food) => (
+                    <Food key={food._id}
+                      data={food}
+                      onSave={this.handleSave}
+                      onEdit={this.handleEdit}
+                      onDelete={this.handleDelete}
+                      onCancel={this.handleCancel}
+                      onLike={this.handleLike} />))}
+                </tbody>
+              </table>
+              <Pagination data={this.state.pagination} onPageChange={this.handlePageChange} />
+            </>
           )}
         </div>
       </>
